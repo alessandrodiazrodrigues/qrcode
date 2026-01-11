7d4b000841aa6dacb6145e45b7283e0b8048d28e
alessandrodiazrodrigues <alessandrorodrigues@cvc.com.br>
2026-01-11 16:06:07 -0300

    Atualizar URL da API V7.5

--- api.js
+++ api.js
@@ -18,7 +18,7 @@
 // ==================================================================================
 
 // *** URL DA API V7.3 - PRODUCAO ***
-const API_URL = 'https://script.google.com/macros/s/AKfycbyQoJmIbYE4SLhRdZV2F_RMUpLSs1wAHbVYPhq8-Cadic5TJeV3uZILUCZZ5gR836eH/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbz6Zxh5unqM33O2EgI81qFvrwhe26rC33okm0AyVMieQoN3f9kBFhH22mqCXRP2SZojpw/exec';
 
 // =================== CONFIGURACAO DE HOSPITAIS V7.0 (9 ATIVOS - 293 LEITOS ENFERMARIA) ===================
 const HOSPITAIS = {
